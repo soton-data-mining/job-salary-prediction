8ad45a19291822aa84ab765cb7be6671e99fc39b
utkuozbulak <utku.ozbulak@gmail.com>
2017-02-08 12:14:39 +0000

    Fixed indentation

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     raw_data = pd.read_csv(raw_data_csv_file_name)
     description_feature = raw_data[['FullDescription']]
     one_hot_encoded_features = get_one_hot_encoded_words(description_feature)
-    # one_hot_encoded_features are a list of 5 lists consisting of 1s and 0s 
-    # If a word appears in the description, this kind of representation will 
+    # one_hot_encoded_features are a list of 5 lists consisting of 1s and 0s
+    # If a word appears in the description, this kind of representation will
     # make it feasible for machine learning
     # We will need to append those lists on final data frame when everything is cleaned
