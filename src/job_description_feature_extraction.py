13b8f3b4ecdafea5ebc53526f8a05e36dddaf0ac
utkuozbulak <utku.ozbulak@gmail.com>
2017-02-08 12:00:38 +0000

    Extracted some features out of description that might help with the prediction

---
+++ src/job_description_feature_extraction.py
@@ -0,0 +1,50 @@
+import string
+import operator
+from collections import Counter
+
+
+def get_one_hot_encoded_words(feature_to_extract):
+    """
+    get_feature_word_count(x) returns a list of tuples which consists of word:count 
+    From those lists, the words appear in the list below ( word_list ) was hand picked
+    Fhey are the word which might have an impact on the salary
+    """
+    word_list = ['excellent','graduate','immediate','junior','urgent']   
+    encoded_features= []
+    for item in word_list:
+        one_hot_encoded_list = []
+        for data in feature_to_extract.iterrows():
+            sentence = data[1][0].lower()# Lower casing the sentence
+            if item in sentence: #
+                one_hot_encoded_list.append(1)
+            else:
+                one_hot_encoded_list.append(0)
+            
+        encoded_features.append(one_hot_encoded_list)
+    return encoded_features
+
+def get_feature_word_count(column_to_count):
+    count_dict = {}
+    for data in column_to_count.iterrows():
+        string_data = str(data)
+        data_digits_removed = ''.join([i for i in string_data if not i.isdigit()])# Remove digits
+        
+        puct_exclude = set(string.punctuation)
+        data_puctuations_removed  = ''.join(ch for ch in data_digits_removed if ch not in puct_exclude)# Remove punctuations
+        data_puctuations_removed = data_puctuations_removed.lower()# Lowercase the string
+        
+        data_as_list = data_puctuations_removed.split()# Turn into list
+        counts_as_dict = Counter(data_as_list)# Turn into dictionary with counts
+        
+        for item in counts_as_dict:
+            if item in count_dict:# If item exists in the dict
+                count_dict[item] = count_dict[item]+1
+            else:# If item doesnt exists
+                count_dict[item]  = 1
+        
+    for item in list(count_dict):
+        if count_dict[item] < len(column_to_count)/200:# Anything below 0.5 is insignificant
+            del count_dict[item]  
+    count_dict = sorted(count_dict.items(), key=operator.itemgetter(0))
+    # Returns a list of tuples which has word:count
+    return count_dict
---
+++ src/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from job_description_feature_extraction import get_one_hot_encoded_words
+
+
+raw_data_csv_file_name = '../data/Train_rev1.csv'
+
+
+if __name__ == "__main__":
+    raw_data = pd.read_csv(raw_data_csv_file_name)
+    description_feature = raw_data[['FullDescription']]
+    one_hot_encoded_features = get_one_hot_encoded_words(description_feature)
+    # one_hot_encoded_features are a list of 5 lists consisting of 1s and 0s 
+    # If a word appears in the description, this kind of representation will 
+    # make it feasible for machine learning
+    # We will need to append those lists on final data frame when everything is cleaned
