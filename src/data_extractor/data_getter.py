3ac095f06bcaadfecd86520dfb46e8173f54d68c
Alex Young <alexdy2007@gmail.com>
2017-02-09 14:21:59 +0000

    Added cities with stop words for title analysis, started title cleaning, Added class to extract data from files to prevent replication of code

---
+++ src/data_extractor/data_getter.py
@@ -0,0 +1,49 @@
+import os
+import csv
+
+class DataGetter():
+
+    CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+    TOWN_DATA = CURRENT_DIR + "/../../towns.txt"
+    CSV_TRAIN_DATA = CURRENT_DIR + "/../../data/Test_rev1.csv"
+
+    @classmethod
+    def get_towns(cls):
+        """
+        get list of towns above 50000
+        :return:list of towns
+        """
+        with open(cls.TOWN_DATA, 'r') as f:
+            cities = []
+            for city in f.readlines():
+                cities.append(city.lower().replace("\n",""))
+            return cities
+
+    @classmethod
+    def get_raw_training_data(cls, header=None):
+        """
+        Get training data from csv
+        :param title: id, Title, FullDesciription ....
+        :return:list of all data, or column list of data if title specified
+        """
+        with open(cls.CSV_TRAIN_DATA, newline='') as f:
+            reader = csv.reader(f, delimiter='\n', quotechar='|')
+            headers = next(reader)[0].split(",")
+            job_test_data = []
+            if header:
+                try:
+                    col_num = headers.index(header)
+                except IndexError as e:
+                    return e("Invalid Header, not found in doc")
+                for row in reader:
+                    job_test_data.append(row[0].split(",")[col_num])
+            else:
+                for row in reader:
+                    job_test_data.append(row[0].split(","))
+
+            return job_test_data
+
+if __name__ == "__main__":
+    #EXAMPLE AND MINI TESTS
+    #DataGetter.get_raw_training_data("Title")
+    print(DataGetter.get_towns())
--- src/exploration/job_titles.py
+++ src/exploration/job_titles.py
@@ -1,45 +1,28 @@
 import os
 import csv
 from bisect import bisect
-from src.helpers.stop_words import get_stopwords
 from collections import Counter
+from src.data_extractor.data_getter import DataGetter
+import RAKE
 
+STOP_WORDS_WIHT_CITIES_PATH = "../../stop_word_with_cities.txt"
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 CSV_TRAIN_DATA = CURRENT_DIR + "/../../data/Test_rev1.csv"
 UNIQUE_JOBS_DATA = CURRENT_DIR + "/../../data/unique_jobs.txt"
 
 
 
-def get_jobs_from_file()
+def pre_process_job_titles():
+        job_titles = DataGetter.get_raw_training_data("Title")
+        job_title_processed = []
+        for i in range(10000):
+            stop_word_remover = RAKE.Rake(STOP_WORDS_WIHT_CITIES_PATH)
+            pre_processed_job_title = job_titles[i]
+            job_title = stop_word_remover.run(str(pre_processed_job_title))
+            job_title_processed.append(job_title)
+            i +=1
+        return job_title_processed
 
-def count_occurances_of_words(header, file=CSV_TRAIN_DATA):
-    with open(file, newline='') as f:
-        reader= csv.reader(f, delimiter='\n', quotechar='|')
-        jobs = []
-        i = 0
-        for row in reader:
-            i+=1
-            jobs.append(str(row).lower())
-            if i>10000:
-                break
-
-        all_words = []
-        job_string = " ".join(jobs)
-        word_counter = Counter(job_string)
-        for item in word_counter.items(): print("{}\t{}".format(*item))
-
-def remove_stop_words():
-    stop_words = get_stopwords()
-
-def read_in_unique_jobs():
-    with open(UNIQUE_JOBS_DATA, 'r') as f:
-        text = f.readline()
-        #Remove white space, and make lower case
-        unique_jobs = [job.lower().replace('"', "").strip() for job in text.split(",")]
-        #Depluralise
-        unique_jobs = [job[:-1] if job[-1]=="s" else job for job in unique_jobs]
-        return unique_jobs
 
 if __name__ == "__main__":
-    unique_jobs = read_in_unique_jobs()
-    count_occurances_of_words("Title")
+    unique_jobs = pre_process_job_titles()
