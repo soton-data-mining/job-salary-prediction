97b1936c6fd70474d5c605a5f44b22be2a565bdc
Alex Young <alexdy2007@gmail.com>
2017-02-09 14:24:33 +0000

    pep8 cleanup

--- src/data_extractor/data_getter.py
+++ src/data_extractor/data_getter.py
@@ -1,8 +1,8 @@
 import os
 import csv
 
-class DataGetter():
 
+class DataGetter:
     CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
     TOWN_DATA = CURRENT_DIR + "/../../towns.txt"
     CSV_TRAIN_DATA = CURRENT_DIR + "/../../data/Test_rev1.csv"
@@ -16,14 +16,14 @@ class DataGetter():
         with open(cls.TOWN_DATA, 'r') as f:
             cities = []
             for city in f.readlines():
-                cities.append(city.lower().replace("\n",""))
+                cities.append(city.lower().replace("\n", ""))
             return cities
 
     @classmethod
     def get_raw_training_data(cls, header=None):
         """
         Get training data from csv
-        :param title: id, Title, FullDesciription ....
+        :param header: id, Title, FullDesciription ....
         :return:list of all data, or column list of data if title specified
         """
         with open(cls.CSV_TRAIN_DATA, newline='') as f:
@@ -34,7 +34,7 @@ class DataGetter():
                 try:
                     col_num = headers.index(header)
                 except IndexError as e:
-                    return e("Invalid Header, not found in doc")
+                    return e
                 for row in reader:
                     job_test_data.append(row[0].split(",")[col_num])
             else:
@@ -43,7 +43,8 @@ class DataGetter():
 
             return job_test_data
 
+
 if __name__ == "__main__":
-    #EXAMPLE AND MINI TESTS
-    #DataGetter.get_raw_training_data("Title")
+    # EXAMPLE AND MINI TESTS
+    # DataGetter.get_raw_training_data("Title")
     print(DataGetter.get_towns())
--- src/exploration/job_titles.py
+++ src/exploration/job_titles.py
@@ -1,7 +1,4 @@
 import os
-import csv
-from bisect import bisect
-from collections import Counter
 from src.data_extractor.data_getter import DataGetter
 import RAKE
 
@@ -11,17 +8,16 @@ CSV_TRAIN_DATA = CURRENT_DIR + "/../../data/Test_rev1.csv"
 UNIQUE_JOBS_DATA = CURRENT_DIR + "/../../data/unique_jobs.txt"
 
 
-
 def pre_process_job_titles():
-        job_titles = DataGetter.get_raw_training_data("Title")
-        job_title_processed = []
-        for i in range(10000):
-            stop_word_remover = RAKE.Rake(STOP_WORDS_WIHT_CITIES_PATH)
-            pre_processed_job_title = job_titles[i]
-            job_title = stop_word_remover.run(str(pre_processed_job_title))
-            job_title_processed.append(job_title)
-            i +=1
-        return job_title_processed
+    job_titles = DataGetter.get_raw_training_data("Title")
+    job_title_processed = []
+    for i in range(10000):
+        stop_word_remover = RAKE.Rake(STOP_WORDS_WIHT_CITIES_PATH)
+        pre_processed_job_title = job_titles[i]
+        job_title = stop_word_remover.run(str(pre_processed_job_title))
+        job_title_processed.append(job_title)
+        i += 1
+    return job_title_processed
 
 
 if __name__ == "__main__":
