1bc8715f11b60aec29723dd2883d04d8c618267d
utkuozbulak <utku.ozbulak@gmail.com>
2017-02-15 13:45:38 +0000

    Updated type comparison with isinstance

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 from job_description_feature_extraction import get_one_hot_encoded_words, get_rake_keywords
-from cleaning_functions import get_one_hot_encoded_feature,
+from cleaning_functions import (get_one_hot_encoded_feature,
                                 get_binary_encoded_feature,
-                                update_location
+                                update_location)
 
 train_raw_data_csv_file_name = '../data/Train_rev1.csv'
 train_normalized_location_file_name = '../data/train_normalised_location.csv'
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     # Region: Binary encoded
     cleaned_binary_encoded_region = get_binary_encoded_feature(updated_region_feature)
 
-    keywords = get_rake_keywords(train_description_feature)
+    #keywords = get_rake_keywords(train_description_feature)
     # one_hot_encoded_features are a list of 5 lists consisting of 1s and 0s
     # If a word appears in the description, this kind of representation will
     # make it feasible for machine learning
