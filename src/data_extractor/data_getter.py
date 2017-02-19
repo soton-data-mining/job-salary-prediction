39a0c71d16403435a019a6a1f52d65caa8482eb2
Ara Hayrabedian <ara.hayrabedian@gmail.com>
2017-02-19 13:27:03 +0000

    - unify data_extractor, DRY and whatnot  - remove now unused get_raw_training_data method

--- src/data_extractor/data_getter.py
+++ src/data_extractor/data_getter.py
@@ -1,5 +1,4 @@
 import os
-import csv
 
 
 class DataGetter:
@@ -9,67 +8,34 @@ class DataGetter:
     CSV_TRAIN_DATA = CURRENT_DIR + "/../../data/Test_rev1.csv"
     UNIQUE_JOB_DATA = CURRENT_DIR + "/../../preprocessing_data/job_roles_unique.txt"
 
+    @staticmethod
+    def _get_data_from_file(filename, lowercase=True):
+        with open(filename, 'r') as f:
+            read_data = []
+            for row in f.readlines():
+                row_data = row.lower() if lowercase else row
+                read_data.append(row_data.replace("\n", ""))
+            return read_data
+
     @classmethod
-    def get_towns(cls):
+    def get_towns(cls, lowercase=True):
         """
         get list of towns above 10000
         :return:list of towns
         """
-        with open(cls.TOWN_DATA, 'r') as f:
-            cities = []
-            for city in f.readlines():
-                cities.append(city.lower().replace("\n", ""))
-            return cities
+        return DataGetter._get_data_from_file(cls.TOWN_DATA, lowercase)
 
     @classmethod
-    def get_stop_word_inc_cities(cls):
+    def get_stop_word_inc_cities(cls, lowercase=True):
         """
         get list of towns above 10000 pop + stop words
         :return:list of towns
         """
-        with open(cls.STOP_WORDS_WITH_CITIES, 'r') as f:
-            words = []
-            for word in f.readlines():
-                words.append(word.lower().replace("\n", ""))
-            return words
+        return DataGetter._get_data_from_file(cls.STOP_WORDS_WITH_CITIES, lowercase)
 
     @classmethod
-    def get_raw_training_data(cls, header=None):
-        """
-        Get training data from csv
-        :param header: id, Title, FullDesciription ....
-        :return:list of all data, or column list of data if title specified
-        """
-        with open(cls.CSV_TRAIN_DATA, newline='') as f:
-            reader = csv.reader(f, delimiter='\n', quotechar='|')
-            headers = next(reader)[0].split(",")
-            job_test_data = []
-            if header:
-                try:
-                    col_num = headers.index(header)
-                except IndexError as e:
-                    return e
-                for row in reader:
-                    job_test_data.append(row[0].split(",")[col_num])
-            else:
-                for row in reader:
-                    job_test_data.append(row[0].split(","))
-
-            return job_test_data
-
-    @classmethod
-    def get_unique_job_roles(cls):
+    def get_unique_job_roles(cls, lowercase=True):
         """
         Get list of unique jobs as specifed by british achieve
         """
-        with open(cls.UNIQUE_JOB_DATA, 'r') as f:
-            jobs = []
-            for job in f.readlines():
-                jobs.append(job.lower().replace("\n", ""))
-            return jobs
-
-
-if __name__ == "__main__":
-    # EXAMPLE AND MINI TESTS
-    # DataGetter.get_raw_training_data("Title")
-    print(DataGetter.get_towns())
+        return DataGetter._get_data_from_file(cls.UNIQUE_JOB_DATA, lowercase)
