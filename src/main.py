154e5e2517ff6fc004867b1b4f0486456928cb0e
utkuozbulak <utku.ozbulak@gmail.com>
2017-04-09 23:11:38 +0100

    Pep-8

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     SupportVectorRegressionModel = SVR()
     SupportVectorRegressionModel.run()
-    
+
     LinearRegressionModel = LinearRegression()
     LinearRegressionModel.run()
 
