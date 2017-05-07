87079533268e3611b98cfdcff023625f7106c459
Alex Young <alexdy2007@gmail.com>
2017-05-07 16:36:17 +0100

    flake8 errors fixed

--- src/part_full_time_feature_extraction.py
+++ src/part_full_time_feature_extraction.py
@@ -1,11 +1,12 @@
+import pandas as pd
+
+
 def get_part_full_time_features(job_describtion_data, part_full_time_data):
     """
-
     :param job_describtion_data: n number of job descriptions
     :param part_full_time_data:  n number of part_full time job data
     :return: n length list containing 1 or 0. 1 for part time, 0 for full time
     """
-
 
     feature_vector = []
     for job in part_full_time_data.iterrows():
@@ -14,17 +15,19 @@ def get_part_full_time_features(job_describtion_data, part_full_time_data):
             if "part" in str(job[1].values[0]).lower():
                 feature_vector.append(1)
             else:
-                if "part time" in str(job_describtion_data._values[idx][0]).lower():
+                if "part time" in str(
+                        job_describtion_data._values[idx][0]).lower():
                     feature_vector.append(1)
                 else:
                     feature_vector.append(0)
     return feature_vector
 
+
 if __name__ == "__main__":
-    import pandas as pd
+
     TRAIN_NORMALIZED_LOCATION_FILE_NAME = '../data/train_normalised_location.csv'
     TRAIN_DATA_CSV_FILE_NAME = '../data/Train_rev1.csv'
     data = pd.read_csv('../data/Train_rev1.csv')
     part_full_vector = get_part_full_time_features(data[["FullDescription"]],
                                                    data[['ContractType']])
-    print("a")
+
